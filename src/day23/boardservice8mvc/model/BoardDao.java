a167773097175facd67962cec7a482829ed831f9
yonggyo1125 <yonggyo00@kakao.com>
2024-11-15 20:30:33 +0900

    day24

--- src/day23/boardservice8mvc/model/BoardDao.java
+++ src/day23/boardservice8mvc/model/BoardDao.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 
 public class BoardDao {
 
+    // 싱글톤
+    private static BoardDao boardDao = new BoardDao();
+    private BoardDao(){}
+    public static BoardDao getInstance(){
+        return boardDao;
+    }
+
+    // 여러개 게시물 저장하는 리스트
     ArrayList<BoardDto> boardDB = new ArrayList<>();
 
     // 1. 게시물 등록 접근 함수
@@ -13,8 +21,8 @@ public class BoardDao {
     }
 
     // 2. 게시물 출력 접근 함수
-    public void boardPrint( ){
-
+    public ArrayList<BoardDto> boardPrint( ){
+        return boardDB;
     }
 
 }
