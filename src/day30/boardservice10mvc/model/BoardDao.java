9871733d83864d9f034f58d5ad05d1b50569e9f6
yonggyo1125 <yonggyo00@kakao.com>
2024-11-25 20:39:29 +0900

    day30

--- src/day30/boardservice10mvc/model/BoardDao.java
+++ src/day30/boardservice10mvc/model/BoardDao.java
@@ -2,6 +2,7 @@ package day30.boardservice10mvc.model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -34,8 +35,27 @@ public class BoardDao {
 
     // 1. 게시물 등록 접근 함수
     public boolean boardWrite( BoardDto boardDto){
-        boardDB.add( boardDto );
-        return true;
+        try {
+            // 1. SQL 작성 , SQL 그대로 작성 하되 데이터가 들어가는 자리는 ? 로 작성
+            String sql = "insert into board( content, writer, pwd) values( ? , ? , ? )";
+            // 2. 작성한 SQL를 DB연동객체의 기재한다. prepare:준비하다 , Statement: 기재하다 , => SQL기재할준비한다.
+            // - 연동된 객체로부터 SQL기재해서 준비된 객체를 PreparedStatement 인터페이스에 대입한다.
+            PreparedStatement ps = conn.prepareStatement(sql);
+            // 3. 기재된 SQL의 매개변수(?)에 값를 대입한다.
+                // ps.setString( ?순서번호 , 대입할데이터 ) : ?에 대입할 데이터가 String 타입일때 사용.
+                // ps.setInt( ?순서번호 , 대입할데이터 ) : ?에 대입할 데이터가 int 타입일때 사용.
+            ps.setString(1, boardDto.getContent()); // 1:SQL내 첫번째 ? 뜻한다  : 첫번째 ? 에 입력받은 게시물 내용 을 대입한다.
+            ps.setString(2, boardDto.getWriter()); // 2:SQL내 두번째 ? 뜻한다 : 두번째 ? 에 입력받은 게시물 작성자를 대입한다.
+            ps.setInt(3, boardDto.getPwd()); // 3:SQL내 세번째 ? 뜻한다 : 세번째 ? 에 입력받은 게시물 비밀번호를 대입한다.
+            // 4. 기재된 SQL 를 실행한다. execute:실행하다 , Update:최신화하다 , => sql 실행후 최신화한다.
+            ps.executeUpdate();
+            // 5. 성공했을때 true 반환
+            return true;
+        }catch ( SQLException e ){
+            e.getMessage(); System.out.println("[ 게시물 등록시 예외발생]");
+        }
+        // 5. 실패 또는 오류 발생시 false 반환
+        return false;
     }
 
     // 2. 게시물 출력 접근 함수
@@ -44,3 +64,10 @@ public class BoardDao {
     }
 
 }
+
+
+
+
+
+
+
